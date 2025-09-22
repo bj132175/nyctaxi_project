f0d16e8891d129ba5e869da1107c06cf7d735ba5
bj132175 <bj132175>
2025-09-22 19:35:50 +0000

    fix the get_target_yyyymm function
    replace '+' with '-'

--- modules/utils/date_utils.py
+++ modules/utils/date_utils.py
@@ -5,7 +5,7 @@ def get_target_yyyymm(months_ago=2):
     '''
     Returns the year-month (yyyy-MM) for the given number of months ago
     '''
-    target_date = date.today() + relativedelta(months=months_ago)
+    target_date = date.today() - relativedelta(months=months_ago)
     return target_date.strftime('%Y-%m')
 
 def get_month_start_n_months_ago(months_ago=2) -> date:
